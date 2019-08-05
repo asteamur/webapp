aceea84d4559036e4d96ec1b9e02f10246947e91
Unknown <miguelangel.alarcos@asteamur.org>
2019-08-05 17:49:06 +0200

    added typeahead

--- src/components/datasheet/Datasheet.vue
+++ src/components/datasheet/Datasheet.vue
@@ -1,14 +1,42 @@
 <template>
   <div>
       <div>
-          Padre: {{father.name}}          
+           <vue-bootstrap-typeahead
+                :data="parents"
+                placeholder="buscar padre"
+                v-model="query"
+                :serializer="s => s.name"
+                @hit="father = $event"
+            />
+          <h3>Padre:</h3>
+          <table>
+          <tr><td>Nombre:</td><td>{{father.name}}</td></tr> 
+          <tr><td>Teléfono:</td><td>{{father.phone}}</td></tr>
+          <tr><td></td><td>
+            <b-button style="float: right;" variant="outline-primary" @click="unlink('father')">Desvincular</b-button>          
+          </td></tr>
+          </table>
       </div>
-      <b-button variant="outline-primary" @click="unlink('father')">Desvincular</b-button>
+      
       <div>
-          Madre: {{mother.name}}          
-      </div>    
-      <b-button variant="outline-primary" @click="unlink('mother')">Desvincular</b-button>
-    <b-form @submit="onSubmit" novalidate>
+          <vue-bootstrap-typeahead
+                :data="parents"
+                placeholder="buscar madre"
+                v-model="query"
+                :serializer="s => s.name"
+                @hit="mother = $event"
+            />
+          <h3>Madre:</h3>
+          <table>
+          <tr><td>Nombre:</td><td>{{mother.name}}</td></tr> 
+          <tr><td>Teléfono:</td><td>{{mother.phone}}</td></tr>
+          <tr><td></td><td>
+            <b-button style="float: right;" variant="outline-primary" @click="unlink('mother')">Desvincular</b-button>          
+          </td></tr>
+          </table> 
+      </div>        
+      <hr/>
+    <b-form @submit="onSubmit" novalidate autocomplete="off">
       <b-form-group
         id="input-group-email"
         label="Dirección de email:"
@@ -25,8 +53,25 @@
           name="email"
           placeholder="Introduce el email"
         ></b-form-input>
+        <!--          autocomplete="email"-->
       </b-form-group>
 
+<!-- -->
+      <b-form-group
+        id="input-group-password"
+        label="password:"
+        label-for="input-password"
+      >
+        <b-form-input
+          id="input-password"
+          type="password"
+          name="password"
+          placeholder="Introduce tu password"
+        ></b-form-input>
+        <!--autocomplete="password"-->
+      </b-form-group>
+
+<!-- -->
       <b-form-group id="input-group-name" label="Your Name:" label-for="input-name">
         <b-form-input
           id="input-name"
@@ -46,17 +91,20 @@
         Enviar email
       </b-button>
     </b-form>
-  </div>
+    <!-- -->
+
+    <!--  -->
+    </div>
 </template>
 
 <script>
 import { mapModel } from 'vuex-bound'
-//import Parent from '@/components/parent/Parent.vue'
+import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
 
 export default {
   name: 'DatasheetForm',  
   components: {
-      //Parent
+      VueBootstrapTypeahead
   },
   created(){
       if(this.$route.params._id){
@@ -65,7 +113,19 @@ export default {
   },
   data(){
     return {
-
+        query: '',
+        parents: [
+            {
+                _id: 'xxx', 
+                name: 'diego alarcos',
+                phone: '616210432'
+            },
+            {
+                _id: 'yyy', 
+                name: 'paquita torrecillas',
+                phone: '968894049'
+            }
+        ]
     }
   },
   methods:{
